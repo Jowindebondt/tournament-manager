f54c753a6f5f05b5fa2fe0f515cb2b2215d14bd2
Jowin de Bondt <jowin.de.bondt@gmail.com>
2024-02-08 10:47:42 +0100

    add comments to databasefixture

--- TournamentManager.Api.Test/Fixtures/DatabaseFixture.cs
+++ TournamentManager.Api.Test/Fixtures/DatabaseFixture.cs
@@ -4,10 +4,20 @@ using TournamentManager.Infrastructure;
 
 namespace TournamentManager.Api.Test;
 
+/// <summary>
+/// Class defining the <see cref="DatabaseFixture"/> containing an in memory database for test purposes.
+/// </summary>
 public class DatabaseFixture : IDisposable
 {
+    /// <summary>
+    /// Reference to the <see cref="ApplicationDbContext"/> which contains an in memory database.
+    /// </summary>
     public ApplicationDbContext DbContext { get; }
 
+    /// <summary>
+    /// Initializing a new instance of <see cref="DatabaseFixture"/>.
+    /// Creates the base setup of the in memory database.
+    /// </summary>
     public DatabaseFixture()
     {
         var options = new DbContextOptionsBuilder<ApplicationDbContext>()
@@ -23,6 +33,7 @@ public class DatabaseFixture : IDisposable
         DbContext.SaveChanges();
     }
 
+    /// <inheritdoc/>
     public void Dispose()
     {
         DbContext.Tournaments.RemoveRange(DbContext.Tournaments);
